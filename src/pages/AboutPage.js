f612c2e2f4c5dde19a759c29862b553b21eab263
Vjola <vjolashtishi@gmail.com>
2021-12-12 19:43:59 +0100

    Router dom setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,13 +1,37 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpg'
 
-const UserContext = React.createContext()
-export const UserProvider = ({ children }) => {
-  return (
-    <UserContext.Provider value='user context'>{children}</UserContext.Provider>
-  )
+const AboutPage = () => {
+  return <h4>about page</h4>
 }
-// make sure use
-export const useUserContext = () => {
-  return useContext(UserContext)
-}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
