de7a003f8045870eedfb8538f54652b53fef2bc1
Vjola <vjolashtishi@gmail.com>
2021-12-18 17:00:19 +0100

    Added services section to home page

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,35 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            High-end devices with superb craftmanship and impressive performance{' '}
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+            reprehenderit sint, dolorem perferendis sunt atque. Tenetur porro
+            temporibus quas delectus rerum amet dignissimos quaerat aliquid,
+            itaque asperiores. Sint, voluptatibus ipsum?
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
