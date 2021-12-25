fc298d8be4da0e3aa0865a93901c01d1cccac68d
Vjola <vjolashtishi@gmail.com>
2021-12-25 16:32:29 +0100

    Setting up product list view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,13 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext()
+  const { filtered_products: products, grid_view } = useFilterContext()
+  if (products.length < 1) {
+    ;<h5 style={{ textTransform: 'none' }}>No products matched your search</h5>
+  }
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
   return <GridView products={products}></GridView>
 }
 
