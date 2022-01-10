c571c987d8a00ac110f63f652ff5e6f1e97ccd51
Vjola <vjolashtishi@gmail.com>
2022-01-10 23:33:11 +0100

    Using auth wrapper

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
-// will remove later
-import { useUserContext } from '../context/user_context'
 
 const PrivateRoute = ({ children }) => {
-  const { myUser } = useUserContext()
+  const { user } = useAuth0()
 
-  if (!myUser) {
+  if (!user) {
     return <Navigate to='/' />
   }
 
