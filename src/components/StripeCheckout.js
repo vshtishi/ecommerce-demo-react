fc9dadf671efaf153bc4a57c7e43deddeef85c53
Vjola <vjolashtishi@gmail.com>
2022-01-17 06:22:24 +0100

    Stripe checkout returns

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -43,7 +43,50 @@ const CheckoutForm = () => {
       },
     },
   }
-  return <h4>hello from Stripe Checkout </h4>
+
+  const createPaymentIntent = async () => {
+    console.log('test')
+  }
+
+  useEffect(() => {
+    createPaymentIntent()
+    //eslint-disable-next-line
+  }, [])
+
+  const handleChange = async (event) => {}
+  const handleSubmit = async (ev) => {}
+
+  return (
+    <div>
+      <form id='payment-form' onSubmit={handleSubmit}>
+        <CardElement
+          id='card-element'
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button disabled={processing || disabled || succeeded} id='submit'>
+          {' '}
+          <span id='button-text'>
+            {processing ? <div className='spinner' id='spinner'></div> : 'Pay'}
+          </span>
+        </button>
+        {/*  Show any error that happens when processing the payment */}
+        {error && (
+          <div className='card-error' role='alert'>
+            {error}
+          </div>
+        )}
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
+          Payment Succeeded, see the result in your{' '}
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            Stripe dashboard.
+          </a>
+          Refresh the page to pay again
+        </p>
+      </form>
+    </div>
+  )
 }
 
 const StripeCheckout = () => {
