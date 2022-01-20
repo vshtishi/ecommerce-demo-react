c43cc0dac63740ca7e50d1a1549493917e9b39a1
Vjola <vjolashtishi@gmail.com>
2022-01-20 06:26:33 +0100

    Added dotenv package

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
+
+const stripe = require('sripe')(process.env.REACT_APP_STRIPE_SECRET_KEY)
+
 exports.handler = async function (event, context) {
+  //check if POST
   if (event.body) {
     const { cart, total_amount } = JSON.parse(event.body)
     return {
