4ef7eb9b3220db44e393d0d4b1d286a42549aca5
Vjola <vjolashtishi@gmail.com>
2022-01-22 19:45:43 +0100

    Payment intent function change

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -6,6 +6,17 @@ exports.handler = async function (event, context) {
   //check if POST
   if (event.body) {
     const { cart, total_amount } = JSON.parse(event.body)
+
+    const getOrderAmount = () => {
+      return total_amount
+    }
+
+    try {
+      const paymentIntent = await stripe.pamentIntents.create({
+        amount: getOrderAmount(),
+      })
+    } catch (error) {}
+
     return {
       statusCode: 200,
       body: JSON.stringify(cart),
